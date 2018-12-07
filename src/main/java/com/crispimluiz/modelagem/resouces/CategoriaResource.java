2483da2a3e73fc5d2c405b6592c9a0c07e742a7a
Crispim Luiz Martins <crispimluiz@live.com>
2018-12-07 14:17:15 -0200

    Primeira Classe Domain - Categoria

---
+++ src/main/java/com/crispimluiz/modelagem/resouces/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.crispimluiz.modelagem.resouces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crispimluiz.modelagem.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> Listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+}
