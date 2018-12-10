140fdb29b872c3025f4f854eeeb1cf8df71dc4e5
Crispim Luiz Martins <crispimluiz@live.com>
2018-12-10 07:19:45 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/crispimluiz/modelagem/services/CategoriaService.java
+++ src/main/java/com/crispimluiz/modelagem/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.crispimluiz.modelagem.domain.Categoria;
 import com.crispimluiz.modelagem.repositories.CategoriaRepository;
+import com.crispimluiz.modelagem.services.Exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,8 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
 }
