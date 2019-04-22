386c176fd5ba069d0f32ebcdc4205214d06868e7
Crispim Luiz Martins <crispimluiz@live.com>
2019-04-22 09:55:43 -0300

    Depois da autenticacao a autorizacao

--- src/main/java/com/crispimluiz/modelagem/Config/SecurytConfig.java
+++ src/main/java/com/crispimluiz/modelagem/Config/SecurytConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.crispimluiz.modelagem.security.JWTAuthenticationFilter;
+import com.crispimluiz.modelagem.security.JWTAuthorizationFilter;
 import com.crispimluiz.modelagem.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurytConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
