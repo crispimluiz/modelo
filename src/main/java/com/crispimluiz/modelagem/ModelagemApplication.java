3aad59a41c0bc26b71b16c2c7b8451ea4efe2183
Crispim Luiz Martins <crispimluiz@live.com>
2019-04-29 15:15:57 -0300

    salvando arquivo na amazon S3, Liberado usuário IBM, Liberando acesso público

--- src/main/java/com/crispimluiz/modelagem/ModelagemApplication.java
+++ src/main/java/com/crispimluiz/modelagem/ModelagemApplication.java
@@ -1,8 +1,11 @@
 package com.crispimluiz.modelagem;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.crispimluiz.modelagem.services.S3Service;
 
 @SpringBootApplication
 public class ModelagemApplication implements CommandLineRunner{
@@ -12,13 +15,16 @@ public class ModelagemApplication implements CommandLineRunner{
 	 *que estava no ModelagemApplication para lá
 	 *Altera o application.properties e cria o application-teste
 	 */
+	
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
+		s3Service.uploadFile("D:\\teste.jpg");
 	}
 }
